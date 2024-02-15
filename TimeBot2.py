f1bc3f7ea9508912923c6b6ddfe9f061863fc361
Nichealys <160175139+Nichealys@users.noreply.github.com>
2024-02-15 15:58:25 -0400

    Add Libraries, add Bot Info Input

---
+++ TimeBot2.py
@@ -0,0 +1,14 @@
+#Initialize the bot, let's get some of these gorgeous libraries going.
+import discordfrom discord.ext import commands
+import json
+import asyncio
+import io
+import os
+import random
+import time
+import hashlib
+
+#Fill in particuliar info here.
+BOT_TOKEN = '' #The Bot Token from Discord.
+AUTHORIZED_USER_ID = #The ID of the user that should have access to admin commands.
+ANNOUNCEMENT_CHANNEL_ID = #The Channel ID where announcements (Minted Green Tokens, Milestones, Lottery, etc..)
