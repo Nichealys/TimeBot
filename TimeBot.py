b783cd9ac66bd7704bbe946ef071e6122c34e0fa
Nichealys <160175139+Nichealys@users.noreply.github.com>
2024-02-15 16:09:15 -0400

    Update TimeBot.py

--- TimeBot.py
+++ TimeBot.py
@@ -1,5 +1,6 @@
 #Initialize the bot, let's get some of these gorgeous libraries going.
-import discordfrom discord.ext import commands
+import discord
+import discord from discord.ext import commands
 import json
 import asyncio
 import io
@@ -9,6 +10,7 @@ import time
 import hashlib
 
 #Fill in particuliar info here.
-BOT_TOKEN = '' #The Bot Token from Discord.
+BOT_TOKEN = '' #The Bot Token from Discord. Enclosed in ''
 AUTHORIZED_USER_ID = #The ID of the user that should have access to admin commands.
 ANNOUNCEMENT_CHANNEL_ID = #The Channel ID where announcements (Minted Green Tokens, Milestones, Lottery, etc..)
+
